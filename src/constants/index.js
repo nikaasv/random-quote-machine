85342e78fcaafe3aeb8a3cc2a7dffa921d31fa2c
Veronika Savikina <veronikasavikina@Veronikas-MacBook-Pro.local>
2019-05-25 21:27:22 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const quotes = [
+  {
+    quote: "quote 1",
+    author: "pippi"
+  },
+  {
+    quote: "quote 2",
+    author: "pippi2"
+  },
+  {
+    quote: "quote 3",
+    author: "pippi3"
+  },
+  {
+    quote: "quote 4",
+    author: "pippi4"
+  },
+  {
+    quote: "quote 5",
+    author: "pippi5"
+  },
+  {
+    quote: "quote 6",
+    author: "pippi6"
+  }
+];
