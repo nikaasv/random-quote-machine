dc9bf474c36895bfd5352bd8b87a220bfaaa7d2f
Veronika Savikina <veronikasavikina@Veronikas-MacBook-Pro.local>
2019-05-26 16:48:07 +0100

    Updates

---
+++ precache-manifest.3419f488d65ab43436255fa4f36e5109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8226561a9869d7f6e89ebbce5fa120a",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5cc56520269640d6461c",
+    "url": "/random-quote-machine/static/css/main.3e86816b.chunk.css"
+  },
+  {
+    "revision": "1373109ea0706e1dc260",
+    "url": "/random-quote-machine/static/js/2.8c8eca74.chunk.js"
+  },
+  {
+    "revision": "5cc56520269640d6461c",
+    "url": "/random-quote-machine/static/js/main.f7e4a181.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
